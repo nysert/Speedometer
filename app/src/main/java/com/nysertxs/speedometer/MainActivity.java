838134a74c25abbbaf2cdd341c1b565231829f67
nysertxs <ponceluisangel96@gmail.com>
2016-05-18 16:40:01 -0500

    added speed functionality

--- app/src/main/java/com/nysertxs/speedometer/MainActivity.java
+++ app/src/main/java/com/nysertxs/speedometer/MainActivity.java
@@ -1,15 +1,23 @@
 package com.nysertxs.speedometer;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
+
+    private TextView speedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +26,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        speedTextView = (TextView)findViewById(R.id.speedTextView);
+
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location == null){
+            Log.e("no gps", "no gps");
+        } else {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1,this);
+        }
     }
 
     @Override
@@ -48,5 +57,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location!=null) {
+            if(location.hasSpeed()){
+
+                float mySpeed = location.getSpeed();
+
+                speedTextView.setText("Current speed: " + mySpeed);
+//                speedTextView.setText("location: " + location.getLatitude() + "," + location.getLongitude());
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
